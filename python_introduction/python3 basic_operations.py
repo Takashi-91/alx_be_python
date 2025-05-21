1f946ef31305ee95ce37ae494a1d3af98f5c3705
Takashi-91 <madimetjaterencechuene@gmail.com>
2025-05-21 12:38:37 +0200

    first commit

---
+++ python_introduction/python3 basic_operations.py
@@ -0,0 +1,23 @@
+
+#Variables
+num1 = 10
+num2 = 5
+
+#Methods
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def message():
+    return "Hello, World!"
+
+#Print Statements
+print(f'Addition of {num1} + {num2} is', add(num1, num2))
+print(f'Subtraction of {num1} - {num2} is', subtract(num1, num2))
+print(f'Multiply of {num1} / {num2} is', multiply(num1, num2))
