af4233874288da89608074e6b4380a12ef56291b
Takashi-91 <madimetjaterencechuene@gmail.com>
2025-05-21 13:13:35 +0200

    add hours to seconds conversion implementation

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+#varibles
+hours=2
+
+# hours to seconds
+def hours_to_seconds(hours):
+    return hours * 60 * 60
+
+# Output
+print(f'{hours} hours is equal to {hours_to_seconds(hours)} seconds')
--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,4 +1,4 @@
-
+#variables          
 length=5
 width=10
 
@@ -6,4 +6,5 @@ width=10
 def area(length, width):
     return length * width
 
+#Output
 print(f'Area of rectangle with length {length} and width {width} is', area(length, width))
