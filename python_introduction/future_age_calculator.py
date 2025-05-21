f41aec5fab366e994501d6b0f7ad525c62bde8e6
Takashi-91 <madimetjaterencechuene@gmail.com>
2025-05-21 15:52:05 +0200

    fix: correct variable name from 'principle' to 'principal' in simple interest calculation add: implement future age calculator script

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+
+# This script defines a function to add two numbers and prints the result.
+age=int(input('Enter your current age: '))
+
+#Methods
+def future_age(age):
+
+    return age + 27
+
+#Output
+print('In 2050 you will be',future_age(age), 'years old.',) 
--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,13 +1,14 @@
 
 #varibles
-principle= 1000
+principal= 1000
 rate= 0.05
 time=3
 
+# principal\s*=\s*1000
 
 #Simple Interest Formula
-def simple_interest(principle, rate, time):
-    return (principle * rate * time) 
+def interest(principal, rate, time):
+    return (principal * rate * time) 
 
-print(f'Simple Interest for {principle}, rate {rate}, time {time} is', simple_interest(principle, rate, time))
+print(f'The simple interest is:',interest(principal, rate, time))
 #Output
