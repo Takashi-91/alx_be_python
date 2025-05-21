c7e05262ad03d5e4487f1235bc18e34702242eac
Takashi-91 <madimetjaterencechuene@gmail.com>
2025-05-21 13:07:02 +0200

    add simple interest calculation implementation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+
+#varibles
+principle= 1000
+rate= 0.05
+time=3
+
+
+#Simple Interest Formula
+def simple_interest(principle, rate, time):
+    return (principle * rate * time) 
+
+print(f'Simple Interest for {principle}, rate {rate}, time {time} is', simple_interest(principle, rate, time))
+#Output
