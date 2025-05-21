92d2ff81a3e508e4efd930ea2db640cfdbe769d7
Takashi-91 <madimetjaterencechuene@gmail.com>
2025-05-21 15:58:19 +0200

    fix: update simple_interest function name and correct interest calculation output

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -5,10 +5,11 @@ rate= 0.05
 time=3
 
 # principal\s*=\s*1000
+interest=principal * rate * time
 
 #Simple Interest Formula
-def interest(principal, rate, time):
+def simple_interest(principal, rate, time):
     return (principal * rate * time) 
 
-print(f'The simple interest is:',interest(principal, rate, time))
+print(f'The simple interest is: {interest}')
 #Output
